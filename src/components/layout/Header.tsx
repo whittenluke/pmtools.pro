52e6e5bff052837b10751e08f1c2b711cde880d0
Luke Whitten <whittenluke@gmail.com>
2024-12-15 20:17:00 -0500

    Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
         <div className="mr-4 flex">
           <a className="mr-6 flex items-center space-x-2" href="/">
-            <span className="font-bold inline-block">PMTools.pro</span>
+            <span className="font-bold inline-block">PMTools</span>
           </a>
         </div>
         <Navigation />
